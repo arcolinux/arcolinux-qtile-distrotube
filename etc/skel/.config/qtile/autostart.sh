5d3b8580404d8d61e0a7c5ae18094e97fe1a0ce3
arcolinuxz <arcolinuxinfo@gmail.com>
2024-02-03 09:10:20 +0100

    first commit

--- etc/skel/.config/qtile/autostart.sh
+++ etc/skel/.config/qtile/autostart.sh
@@ -2,59 +2,20 @@
 
 COLORSCHEME=DoomOne
 
-function run {
-  if ! pgrep $1 ;
-  then
-    $@&
-  fi
-}
 
-#Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
-#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
-#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
-#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-#autorandr horizontal
+### AUTOSTART PROGRAMS ###
+lxsession &
+picom --daemon &
+/usr/bin/emacs --daemon &
+nm-applet &
+"$HOME"/.screenlayout/layout.sh &
+sleep 1
+conky -c "$HOME"/.config/conky/qtile/01/"$COLORSCHEME".conf || echo "Couldn't start conky."
 
-#$HOME/.config/polybar/launch.sh &
-
-#change your keyboard if you need it
-#setxkbmap -layout be
-
-# keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
-
-# if [ $keybLayout = "be" ]; then
-#   run sxhkd -c ~/.config/qtil/sxhkd/sxhkdrc-azerty &
-# else
-# run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
-# fi
-
-#Some ways to set your wallpaper besides variety or nitrogen
-feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
-feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-#wallpaper for other Arch based systems
-#feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
-#feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
-
-dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
-xsetroot -cursor_name left_ptr &
-
-run variety &
-run nm-applet &
-run pamac-tray &
-run xfce4-power-manager &
-numlockx on &
-blueberry-tray &
-picom --config $HOME/.config/qtile/scripts/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
-run volumeicon &
-#nitrogen --restore &
-#run caffeine &
-#run vivaldi-stable &
-#run firefox &
-#run thunar &
-#run dropbox &
-#run insync start &
-#run discord &
-#run spotify &
+### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
+# 1. Uncomment to restore last saved wallpaper
+xargs xwallpaper --stretch < ~/.cache/wall &
+# 2. Uncomment to set a random wallpaper on login
+# find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
+# 3. Uncomment to set wallpaper with nitrogen
+# nitrogen --restore &
--- etc/skel/.config/qtile/colors.py
+++ etc/skel/.config/qtile/colors.py
@@ -2,19 +2,6 @@
 # Then, you can use RGBA color codes to add transparency to the colors below.
 # For ex: colors = [["#282c34ee", "#282c34dd"], ...
 
-Arco = [
-	["#282c34", "#282c34"],
-    ["#1c1f24", "#1c1f24"],
-    ["#dfdfdf", "#dfdfdf"],
-    ["#ff6c6b", "#ff6c6b"],
-    ["#98be65", "#98be65"],
-    ["#da8548", "#da8548"],
-    ["#51afef", "#51afef"],
-    ["#c678dd", "#c678dd"],
-    ["#46d9ff", "#46d9ff"],
-    ["#a9a1e1", "#a9a1e1"]
-    ]
-
 DoomOne = [
     ["#282c34", "#282c34"], # bg
     ["#bbc2cf", "#bbc2cf"], # fg
