e4f68a7945852699f00221a6d732c4b77eb732be
arcolinuxz <arcolinuxinfo@gmail.com>
2024-02-04 08:07:36 +0100

    autostart in scripts

--- etc/skel/.config/qtile/config.py
+++ etc/skel/.config/qtile/config.py
@@ -534,7 +534,7 @@ wl_input_rules = None
 @hook.subscribe.startup_once
 def start_once():
     home = os.path.expanduser('~')
-    subprocess.call([home + '/.config/qtile/autostart.sh'])
+    subprocess.call([home + '/.config/qtile/scripts/autostart.sh'])
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
