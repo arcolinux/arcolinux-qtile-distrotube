dbba11dee8afe8c5867eed67b7be15403d1ffed9
arcolinuxz <arcolinuxinfo@gmail.com>
2025-01-11 10:46:52 +0100

    update

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -63,6 +63,7 @@ run nm-applet &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
+picom --config $HOME/.config/qtile/scripts/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 
@@ -77,6 +78,6 @@ run volumeicon &
 #run dropbox &
 #run insync start &
 #run spotify &
-#run code &
 #run telegram-desktop &
 #run /usr/bin/octopi-notifier &
+#run code &
